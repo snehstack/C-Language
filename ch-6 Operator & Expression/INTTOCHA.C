98a3048fdffe36ca7d9c2be3a28c38f8b8c41cf2
Sneh <snehsohagiya117@gmail.com>
2026-02-12 12:01:08 +0530

    ch-7

---
+++ ch-6 Operator & Expression/INTTOCHA.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+#define pf  printf
+
+main()
+
+{
+
+    float a=12.21,b=64.18;
+
+    clrscr();
+
+    pf("Number\t:%f",a);
+    pf("Number\t:%d",(int)a);
+
+
+    getch();
+
+
+}
---
+++ ch-7 Control Stracture/ODDEVENN.C
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <conio.h>
+#define pf printf
+#define sc scanf
+
+
+main()
+
+{
+
+    int num;
+
+    clrscr();
+
+    pf("Enter Number\t:");
+    sc("%d",&num);
+
+    //First Method
+
+    if(num%2==0)
+    {
+
+	pf("This Number is Even");
+
+    }
+
+    else
+
+    {
+
+       pf("This Number is Odd");
+
+    }
+
+    pf("\n\n\n\n");
+
+    //Second Method
+
+    if((num/2)*2==num)
+
+    {
+
+      pf("This Number is Even");
+
+    }
+
+    else
+
+    {
+
+      pf("This Number is Odd");
+
+    }
+
+    getch();
+
+}
